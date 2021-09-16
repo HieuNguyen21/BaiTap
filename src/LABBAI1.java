5e41aaa8659c7bf057dba084f7ea8325aad9207b
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2021-09-16 14:25:02 +0700

    BaiTap

---
+++ src/LABBAI1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LABBAI1 {
+  public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.print("Họ tên: ");
+
+     String hoten = scanner.nextLine();
+
+     System.out.print("Điểm TB: "); 
+      double diemTB = scanner.nextDouble(); 
+      System.out.printf("%s %f điểm", hoten, diemTB); 
+          
+    }
+
+}
