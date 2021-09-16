ad073e709fe6a81a3515f68c78a5652e52e8834c
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2021-09-16 13:01:06 +0700

    BaiTap

---
+++ src/Thuvien.java
@@ -0,0 +1,12 @@
+public class Thuvien {
+    public static void main(String[] args){
+        int soNguyen = 7;
+
+        double ketQua;
+        ketQua = Math.sqrt(soNguyen);
+
+        System.out.println("Kết quả cân "+ ketQua);
+
+    }
+    
+}
