9930950e739754cd1549c567b66247e306d27bd6
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2021-09-23 06:48:25 +0700

    luudautien

--- src/Lab2/bai2.java
+++ src/Lab2/bai2.java
@@ -1,26 +1,29 @@
 package Lab2;
 
+import java.util.Scanner;
+
 public interface bai2 {
-    public static void main(String[] args)
-     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("nhap a:");
-        int a = scanner.nextDouble();
-        System.out.print("Nhap b: ");
-        int b = scanner.nextDouble();
-        System.out.print("Nhap c: ");
-        int c = scanner.nextDouble();
-        System.out.println("Ppt bac hai: " + a + "x^2 + " + b + "x + " + c + " = 0");
-        delta = Math.pow(b, 2) - 4 * a * c;
-        if (deltail < 0) {
-            System.out.println("Phuong trinh vo nghiem");
-        } else if (deltail == 0) {
-           double x1 = -b / (2 * a);
-            System.out.println("Phuong trinh có 1 nghiem là x1 = x2 = " + x1);
-        } else {
-           double x1 = (-b + Math.sqrt(deltail)) / (2 * a);
-           double x2 = (-b - Math.sqrt(deltail)) / (2 * a);
-            System.out.println("Phuong trinh co 2 nghiem x1 = " + x1 + " và x2 = " + x2);
+    public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Phuong trinh bac 2 co dang: ax^x+bx+c=0");
+            System.out.print("nhap he so a: ");
+            int a = scanner.nextInt();
+            System.out.print("nhap he so b: ");
+            int b = scanner.nextInt();
+            System.out.print("nhap he so c: ");
+            int c = scanner.nextInt();
+            double delta = (Math.pow(b,2)-(4*a*c));
+            if (delta<0) {
+                System.out.println("PT  vo nghiem");
+            } else if(delta==0) {
+                double x1=x2= (-b/(2*a));
+                System.out.println("PT co nghiem kep x1 = x2= " + x1);
+            }
+            else 
+            {
+               Double x1 = (float) ((-b + sqrt(delta))/(2*a));
+               Double x2 = (float) ((-b - sqrt(delta))/(2*a));
+                System.out.println("PT co 2 nghiem x1 = " + x1 + "x2 = " + x2);
+            }
         }
     }
-}
