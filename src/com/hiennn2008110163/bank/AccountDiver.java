ed0de9834b312448e075b90ddf8e310f6602893a
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2021-10-07 07:13:01 +0700

    landautien

--- src/com/hiennn2008110163/bank/AccountDiver.java
+++ src/com/hiennn2008110163/bank/AccountDiver.java
@@ -1,10 +1,6 @@
 package com.hiennn2008110163.bank;
-
 public class AccountDiver {
     public static void main(String[] args) {
-		// Account account = new Account();
-		// account.create();
-		// account.show();
 		Customer customer = new Customer();
 		customer.getName();
 		customer.getAddress();
