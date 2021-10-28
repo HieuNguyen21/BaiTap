7c925782975158b073779b3291d417eaf88f5d7a
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2021-10-28 07:55:27 +0700

    landautien->tuan 7

--- src/com/hiennn2008110163/lab7/animail/Animail.java
+++ src/com/hiennn2008110163/lab7/animail/Animail.java
@@ -1,5 +1,22 @@
 package com.hiennn2008110163.lab7.animail;
 
 public class Animail {
-    
+    protected String picture;
+	protected String food;
+	protected int hunger;
+	protected String boundaries;
+	protected int location;
+
+	protected void makeNoise(){
+		System.out.println("Động vật kêu!!");
+	}
+	protected void eat(){
+		System.out.println("Ăn một cách bình thường!!");
+	}
+	protected void sleep(){
+		System.out.println("Đang ngủ !!");
+	}
+	protected void roam(){
+		System.out.println("Đang đi loanh quanh!!");
+	}
 }
