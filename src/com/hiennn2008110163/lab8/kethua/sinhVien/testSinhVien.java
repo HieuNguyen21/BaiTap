b1a145d6a8780a478a2a22e2fb9f29fcfd4907cf
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2021-11-03 20:44:15 +0700

    lab 8->tuần 8 và những bài sửa

---
+++ src/com/hiennn2008110163/lab8/kethua/sinhVien/testSinhVien.java
@@ -0,0 +1,12 @@
+package com.hiennn2008110163.lab8.kethua.sinhVien;
+
+public class testSinhVien {
+    public static void main(String[] args) {
+        SinhVienIT it = new SinhVienIT();
+        it.getDiem();
+        it.getHocLuc();
+        sinhVienKT kt = new sinhVienKT ();
+        kt.getDiem();
+        kt.getHocLuc();
+    }
+}
