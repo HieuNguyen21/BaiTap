a022e0ed6bfe46ef412bb0fa7b33fde866461a1d
HieuNguyen21 <nnguyenngochieu1@gmail.com>
2021-11-04 09:04:16 +0700

    kiemtragiuaky

--- src/com/hiennn2008110163/kiemtragiuaki/test.java
+++ src/com/hiennn2008110163/kiemtragiuaki/test.java
@@ -1,8 +1,17 @@
 package com.hiennn2008110163.kiemtragiuaki;
 
 public class test {
-    hocVien hv =new hocVien();
-    hv.getDiem();
-    
+    aNguoi nguoi = new aNguoi("Devil", 0, 0, 0);
+    nguoi.getDiaChi();
+    nguoi.getHoTen();
+    NhanVien nhanvien = new NhanVien("Angel",0,0);
+    nhanvien.getDiaChi();
+    nhanvien.getsalary();
+    KhachHang khachhang = new KhachHang(null, 0, 0);
+    khachhang.gettriGiaHoaDon();
+    HocVien hocvien = new HocVien(null, 0, 0, 0);
+    hocvien.getdiemtrungbinh();
+}
+}
 
 }
